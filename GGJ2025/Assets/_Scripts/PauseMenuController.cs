6177cec929914a067588921d84174fa511f8d81f
Alejo Gaitán <thy.gaitan@gmail.com>
2025-01-30 20:57:18 -0300

    Fixed Credits

--- GGJ2025/Assets/_Scripts/PauseMenuController.cs
+++ GGJ2025/Assets/_Scripts/PauseMenuController.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class PauseMenuController : MonoBehaviour
